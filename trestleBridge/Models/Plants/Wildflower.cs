fa5bffc6b6be1f26e96d6e136667c95dd9bb0a66
rootmyth <rootmyth@gmail.com>
2022-08-09 18:11:48 -0500

    Created Wildflower Class

---
+++ trestleBridge/Models/Plants/Wildflower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trestleBridge.Models.Plants
+{
+    internal class Wildflower
+    {
+        private Guid _id = Guid.NewGuid();
+        private double _seedsProduced = 36.1;
+        private string _shortId
+        {
+            get
+            {
+                return this._id.ToString().Substring(this._id.ToString().Length - 6);
+            }
+        }
+        public double WaterPerDay { get; set; } = 3.8;
+        public string Type { get; } = "Wildflower";
+        public void Absorb()
+        {
+            Console.WriteLine($"Wildflower plants {this._shortId} just absorbed {this.WaterPerDay} gallons of water");
+        }
+        public double Reap()
+        {
+            return _seedsProduced;
+        }
+        public override string ToString()
+        {
+            return $"Sunflower plants {this._shortId}. Wild!";
+        }
+    }
+}
