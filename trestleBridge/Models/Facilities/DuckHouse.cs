19fde6cd4aaf3709c9bd243134526cdc3ea0b0d8
sarahemaginnis <sarahemaginnis@gmail.com>
2022-08-09 18:49:18 -0500

    Built Duck House class

--- trestleBridge/Models/Facilities/DuckHouse.cs
+++ trestleBridge/Models/Facilities/DuckHouse.cs
@@ -1,1 +1,46 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using trestleBridge.Interfaces;
+
+namespace trestleBridge.Models.Facilities
+{
+    public class DuckHouse : IFacility<IGrazing>
+    {
+        private double _capacity = 12;
+        private Guid _id = Guid.NewGuid();
+
+        private List<IGrazing> _animals = new List<IGrazing>();
+
+        public double Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+        }
+
+        public void AddResource(IGrazing animal)
+        {
+            // TODO: implement this...
+            throw new NotImplementedException();
+        }
+
+        public void AddResource(List<IGrazing> animals)
+        {
+            // TODO: implement this...
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
+            output.Append($"Grazing field {shortId} has {this._animals.Count} animals\n");
+            this._animals.ForEach(a => output.Append($"   {a}\n"));
+            return output.ToString();
+        }
+    }
+}
+
