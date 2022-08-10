b59ef5ba30d0364c7e5bb8356fa2c87a792a46fc
rootmyth <rootmyth@gmail.com>
2022-08-10 12:43:28 -0500

    Created class to choose either plant field when planting sunflowers.

---
+++ trestleBridge/Actions/ChooseNaturalOrPlowed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using trestleBridge.Interfaces;
+
+namespace trestleBridge.Actions
+{
+    internal class ChooseNaturalOrPlowed
+    {
+        public static void CollectInput(Farm farm, IFlower plant)
+        {
+            // Console.Clear();
+            for (int i = 0; i < farm.NaturalFields.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. Natural Field");
+            }
+            for (int i = farm.NaturalFields.Count + 1; i < farm.PlowedFields.Count + farm.NaturalFields.Count + 1; i++)
+            {
+                Console.WriteLine($"{i + 1}. Plowed Field");
+            }
+            Console.WriteLine();
+            // How can I output the type of animal chosen here?
+            Console.WriteLine($"Place {plant.Type.ToString().ToLower()} where?");
+
+            Console.Write("> ");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            if (choice <= farm.NaturalFields.Count)
+            {
+                farm.NaturalFields[choice].AddResource(plant);
+            }
+            else if (choice > farm.NaturalFields.Count)
+            {
+                farm.PlowedFields[choice - farm.NaturalFields.Count].AddResource(plant);
+            }
+
+            /*
+                Couldn't get this to work. Can you?
+                Stretch goal. Only if the app is fully functional.
+             */
+            // farm.PurchaseResource<IGrazing>(animal, choice);
+        }
+    }
+}
