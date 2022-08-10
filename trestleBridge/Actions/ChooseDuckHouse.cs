d126d911de6c5ab675cdca02e0c0434e0a38f92c
McKenzie Byrd <mckenziebyrd@me.com>
2022-08-10 13:04:27 -0500

    added duck and chicken houses. and swtich statement in purchase stock

--- trestleBridge/Actions/ChooseDuckHouse.cs
+++ trestleBridge/Actions/ChooseDuckHouse.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using trestleBridge.Interfaces;
+
 namespace trestleBridge.Actions
 {
 	public class ChooseDuckHouse
 	{
-		public ChooseDuckHouse()
-		{
-		}
+		
+            public static void CollectInput(Farm farm, IGrazing animal)
+            {
+                // Console.Clear();
+                for (int i = 0; i < farm.DuckHouses.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. Duck House");
+                }
+                Console.WriteLine();
+                // How can I output the type of animal chosen here?
+                Console.WriteLine($"Place {animal.Type.ToString().ToLower()} where?");
+
+                Console.Write("> ");
+                int choice = Int32.Parse(Console.ReadLine());
+
+                farm.DuckHouses[choice].AddResource(animal);
+
+                /*
+                    Couldn't get this to work. Can you?
+                    Stretch goal. Only if the app is fully functional.
+                 */
+                // farm.PurchaseResource<IGrazing>(animal, choice);
+            }
+        
 	}
 }
 
--- trestleBridge/Actions/PurchaseStock.cs
+++ trestleBridge/Actions/PurchaseStock.cs
@@ -24,7 +24,25 @@ namespace trestleBridge.Actions
             switch (Int32.Parse(choice))
             {
                 case 1:
+                    ChooseChickenHouse.CollectInput(farm, new Chicken());
+                    break;
+                case 2:
                     ChooseGrazingField.CollectInput(farm, new Cow());
+                    break;
+                case 3:
+                    ChooseDuckHouse.CollectInput(farm, new Duck());
+                    break;
+                case 4:
+                    ChooseGrazingField.CollectInput(farm, new Goat());
+                    break;
+                case 5:
+                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+                    break;
+                case 6:
+                    ChooseGrazingField.CollectInput(farm, new Pig());
+                    break;
+                case 7:
+                    ChooseGrazingField.CollectInput(farm, new Sheep());
                     break;
                 default:
                     break;
